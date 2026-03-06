78a6d563c3225098cfd6456b08d9d30a430b2342
Drakoxw <developer17@sinergiaonline.com>
2026-03-06 00:40:58 -0500

    feat: implementar soporte inicial de PWA (Service Worker, Manifest e Iconos)
    - Registro de `provideServiceWorker` en `app.config.ts`.
    - Configuración de `ngsw-config.json` para el Service Worker.
    - Adición de `manifest.webmanifest` y assets de iconos.
    - Actualización de `angular.json` y `package.json` para soportar capacidades de PWA.
    - Inclusión del manifest en `index.html`.

--- aura-frontend/src/app/app.config.ts
+++ aura-frontend/src/app/app.config.ts
@@ -1,4 +1,4 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideZoneChangeDetection, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { providePrimeNG } from 'primeng/config';
@@ -17,6 +17,7 @@ import {
   authInterceptor,
   DateInterceptorInterceptor,
 } from './core/interceptors/';
+import { provideServiceWorker } from '@angular/service-worker';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -41,6 +42,9 @@ export const appConfig: ApplicationConfig = {
       provide: HTTP_INTERCEPTORS,
       useClass: GlobalInterceptor,
       multi: true,
-    },
+    }, provideServiceWorker('ngsw-worker.js', {
+            enabled: !isDevMode(),
+            registrationStrategy: 'registerWhenStable:30000'
+          }),
   ],
 };
