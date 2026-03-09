d6ccca4fc5e9d6472a045b70fec852f71c900ae0
camiloolea20077 <112830645+camiloolea20077@users.noreply.github.com>
2026-03-09 10:12:55 -0500

    feat: se ajusta ruta

--- aura-pos/src/main/java/com/cloud_technological/aura_pos/config/WebConfig.java
+++ aura-pos/src/main/java/com/cloud_technological/aura_pos/config/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // Aplica a todos los endpoints
-                .allowedOrigins("http://localhost:4200", "https://aura-post.vercel.app/", "https://www.aura-pos.tech/") // Permite
+                .allowedOrigins("http://localhost:4200", "https://aura-post.vercel.app/", "https://www.aura-pos.tech/","https://aura-pos.tech/") // Permite
                                                                                                                         // el
                                                                                                                         // frontend
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH")
