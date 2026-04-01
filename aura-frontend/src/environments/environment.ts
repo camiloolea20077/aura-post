3e5e609512b1e533782a232ea23c6554d5243e18
Drakoxw <dragon12xw@gmail.com>
2026-03-31 22:51:17 -0500

    fix: env

--- aura-frontend/src/environments/environment.ts
+++ aura-frontend/src/environments/environment.ts
@@ -1,5 +1,4 @@
 export const environment = {
   production: false,
-  // apiUrl: 'http://localhost:9001/api/',
-  apiUrl: 'https://back-old-dev.cloudtecnology.cloud/api/',
+  apiUrl: 'http://localhost:9001/api/',
 };
